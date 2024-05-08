e8aa327634ffb2f3b60c005f21833ac9f252f727
Ajay Prasad <ajay.prasad@outlook.in>
2024-05-08 12:56:39 +0530

    implemented infinite scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import JobListing from './component/JobListing'
 
 const App = () => {
+  const [length, setLength] = useState(15)
+
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY < document.body.offsetHeight) {
+      return
+    }
+    setLength((x) => {
+      return x + 15
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+
   return (
     <div className='app-root'>
-      <JobListing />
+      <div className='listing-container'>
+        <div className='listing-grid grid-cols'>
+          {
+            (() => {
+              const cards = []
+              for (let i = 0; i < length; i++) {
+                cards.push(<JobListing key={i} />)
+              }
+              return cards
+            })()
+          }
+        </div>
+      </div>
     </div>
   )
 }
